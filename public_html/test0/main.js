d4f69cf98346cc810f711947b0295e9b2b445813
Aaron Whyte <aaron.whyte@gmail.com>
2014-03-23 22:50:03 -0700

    scroll-free basic touch handling on Android and iOS

--- public_html/test0/main.js
+++ public_html/test0/main.js
@@ -1,12 +1,19 @@
+var canvas = null;
+var ctx = null;
+
 function main() {
+  canvas = document.querySelector('#canvas');
+  ctx = canvas.getContext("2d");
+
   window.addEventListener("resize", function(){
     resizeCanvas();
   });
   resizeCanvas();
+
+  initGestureListeners();
 }
 
 function resizeCanvas() {
-  var canvas = document.querySelector('#canvas');
   var w = window.innerWidth;
   var h = window.innerHeight;
   canvas.style.width = w + "px";
@@ -19,4 +26,20 @@ function resizeCanvas() {
   ctx.beginPath();
   ctx.rect(0, 0, w, h);
   ctx.stroke();
+}
+
+function initGestureListeners() {
+  document.body.addEventListener("touchstart", touchDraw);
+  document.body.addEventListener("touchmove", touchDraw);
+  document.body.addEventListener("touchend", touchDraw);
+}
+
+function touchDraw(evt) {
+  for (var i = 0; i < event.touches.length; i++) {
+    var touch = event.touches[i];
+    ctx.beginPath();
+    ctx.arc(touch.pageX, touch.pageY, 20, 0, 2*Math.PI, true);
+    ctx.fill();
+    ctx.stroke();
+  }
 }
