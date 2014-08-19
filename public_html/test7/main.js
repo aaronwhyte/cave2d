07549684163e5f247c99e4f6b36dd9bc2d768e57
Aaron Whyte <aaron.whyte@gmail.com>
2014-08-18 23:33:21 -0700

    test7 small changes
    mostly redraw tweaks and some color changes

--- public_html/test7/main.js
+++ public_html/test7/main.js
@@ -4,18 +4,17 @@ var grid, squares, vec;
 
 var pointers = {};
 
-SQUARE_PAD = 0.1;
+var DIRTY_RECT_PAD = 1;
 
 function main() {
   canvas = document.querySelector('#canvas');
   ctx = canvas.getContext("2d");
   viewport = new Viewport(canvas);
   camera = new Camera();
-  camera.setPanXY(2000, 2000);
   camera.setZoom(1/100);
   camera.setRotation(0);
 
-  grid = new QuadTreeGrid(100, 8);
+  grid = new QuadTreeGrid(150, 8);
 
   window.addEventListener("resize", function(){
     resizeCanvas();
@@ -40,7 +39,7 @@ function drawAll() {
   grid.getSquaresOfColor(2, squares);
 
   ctx.save();
-  ctx.fillStyle = "#000";
+  ctx.fillStyle = FILL_STYLES[VOID];
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.restore();
 
@@ -129,14 +128,20 @@ function paintHall(p1, opt_p2) {
   var segment = new Segment(p1, p2);
   var painter = new HallPillPainter(segment, 4, 3);
   grid.paint(painter);
-  drawDirtyRect(painter.getBoundingRect());
+  drawDirtyRect(painter.getBoundingRect().pad(DIRTY_RECT_PAD));
 }
 
-var FILL_STYLES = ["#000", "#ddd", "#666"];
+var VOID = 0;
+var FLOOR = 1;
+var WALL = 2;
+
+var FILL_STYLES = ["#000", "#fff", "#66f"];
 
 function drawDirtyRect(rect) {
   ctx.save();
-  ctx.fillStyle = "#000";
+  viewport.transform(ctx);
+  camera.transform(ctx);
+  ctx.fillStyle = FILL_STYLES[VOID];
   ctx.fillRect(rect[0] - rect[2], rect[1] - rect[3], rect[2] * 2, rect[3] * 2);
   ctx.restore();
 
@@ -147,20 +152,20 @@ function drawSquares(squares) {
   ctx.save();
   viewport.transform(ctx);
   camera.transform(ctx);
-  ctx.lineWidth = 0.2;
-
-  for (var color = 1; color < 3; color++) {
+  var drawOrder = [WALL, FLOOR];
+  var pad = 5.5 * camera.getZoom();
+  for (var drawIndex = 0; drawIndex < drawOrder.length; drawIndex++) {
+    var color = drawOrder[drawIndex];
     for (var i = 0; i < squares.length; i++) {
       var s = squares[i];
       // color, centerX, centerY, radius
       if (s[0] != color) continue;
       var x = s[1];
       var y = s[2];
-      var r = s[3];
+      var r = s[3] + pad;
       ctx.fillStyle = FILL_STYLES[color];
       ctx.strokeStyle = FILL_STYLES[color];
       ctx.fillRect(x - r, y - r, r * 2, r * 2);
-      ctx.strokeRect(x - r, y - r, r * 2, r * 2);
     }
   }
   ctx.restore();
