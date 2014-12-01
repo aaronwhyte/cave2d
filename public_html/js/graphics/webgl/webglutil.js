b5eab1143a2149ebf470a8cc2776dd31210e4919
Aaron Whyte <aaron.whyte@gmail.com>
2014-11-30 22:32:33 -0800

    Saving some test15 work
    64x64 background rects and one foreground rect.

---
+++ public_html/js/graphics/webgl/webglutil.js
@@ -0,0 +1,131 @@
+/**
+ * Gets a WebGL context from a canvas
+ * @param canvas
+ * @param paramObj
+ * @return {WebGLRenderingContext}
+ */
+function getWebGlContext(canvas, paramObj) {
+  if (paramObj) {
+    return canvas.getContext('experimental-webgl', paramObj) || canvas.getContext('webgl', paramObj);
+  } else {
+    return canvas.getContext('experimental-webgl') || canvas.getContext('webgl');
+  }
+}
+
+/**
+ * Creates a program from 2 script tags
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} vertexShaderId  vertex shader script tag ID
+ * @param {string} fragmentShaderId  fragment shader script tag ID
+ * @return {!WebGLProgram}
+ */
+function createProgramFromScripts(gl, vertexShaderId, fragmentShaderId) {
+  var vertexShader = createShaderFromScript(gl, vertexShaderId, gl.VERTEX_SHADER);
+  var fragmentShader = createShaderFromScript(gl, fragmentShaderId, gl.FRAGMENT_SHADER);
+  return createProgram(gl, vertexShader, fragmentShader);
+}
+
+
+/**
+ * Creates a shader from the content of a script tag.
+ * @param {!WebGLRenderingContext} gl
+ * @param {string} scriptId
+ * @param {number} shaderType  gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
+ * @return {!WebGLShader}
+ */
+function createShaderFromScript(gl, scriptId, shaderType) {
+  var shaderScript = document.getElementById(scriptId);
+  var shaderSource = shaderScript.text;
+  return compileShader(gl, shaderSource, shaderType);
+}
+
+
+/**
+ * Creates and compiles a shader.
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} shaderSource The GLSL source code for the shader.
+ * @param {number} shaderType The type of shader, VERTEX_SHADER or
+ *     FRAGMENT_SHADER.
+ * @return {!WebGLShader} The shader.
+ */
+function compileShader(gl, shaderSource, shaderType) {
+  var shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+  }
+  return shader;
+}
+
+
+/**
+ * Creates a program from 2 shaders.
+ * @param {!WebGLRenderingContext} gl
+ * @param {!WebGLShader} vertexShader
+ * @param {!WebGLShader} fragmentShader
+ * @return {!WebGLProgram}
+ */
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw 'program filed to link:' + gl.getProgramInfoLog(program);
+  }
+  return program;
+}
+
+
+// Make WebStorm 8 happy
+if (!'WebGLRenderingContext' in window) {
+  throw 'WebGLRenderingContext undefined!';
+  (function(){
+    var f = function(){};
+    window.WebGLRenderingContext = {
+      createShader: f,
+      shaderSource: f,
+      compileShader: f,
+      COMPILE_STATUS: 1,
+      getShaderParameter: f,
+      getShaderInfoLog: f,
+      createProgram: f,
+      attachShader: f,
+      linkProgram: f,
+      getProgramParameter: f,
+      getProgramInfoLog: f,
+      LINK_STATUS: 1,
+      VERTEX_SHADER: 1,
+      FRAGMENT_SHADER: 1,
+      useProgram: f,
+
+      clearColor: f,
+      enable: f,
+      DEPTH_TEST: 1,
+      getAttribLocation: f,
+      enableVertexAttribArray: f,
+      getUniformLocation: f,
+      uniformMatrix4fv: f,
+      createBuffer: f,
+      ARRAY_BUFFER: 1,
+      bindBuffer: f,
+      bufferData: f,
+      COLOR_BUFFER_BIT: 1,
+      DEPTH_BUFFER_BIT: 1,
+      FLOAT: 1,
+      TRIANGLES: 1,
+      TRIANGLE_STRIP: 1,
+      vertexAttribPointer: f,
+      drawArrays: f,
+      STATIC_DRAW: 1,
+      DYNAMIC_DRAW: 1,
+      STREAM_DRAW: 1,
+      uniform4f: f,
+      uniform3fv: f
+    };
+
+    window.WebGLShader = f;
+    window.WebGLProgram = f;
+  })();
+}
---
+++ public_html/test15/main.js
@@ -0,0 +1,124 @@
+
+var canvas, gl, program;
+var bgVertBuff, bgColorBuff;
+
+var CELLS_X = 64;
+var CELLS_Y = 64;
+var CELLS_COUNT = CELLS_X * CELLS_Y;
+
+function main() {
+  canvas = document.querySelector('#canvas');
+
+  gl = getWebGlContext(canvas, {
+    alpha: false,
+    antialias: true
+  });
+  program = createProgramFromScripts(gl, 'vertex-shader', 'fragment-shader');
+  gl.enable(gl.DEPTH_TEST);
+  gl.useProgram(program);
+
+  initBackgroundData(gl);
+
+  loop();
+}
+
+
+function initBackgroundData(gl) {
+  var verts = [];
+  var colors = [];
+
+  for (var y = 0; y < CELLS_Y; y++) {
+    for (var x = 0; x < CELLS_X; x++) {
+      var px = 2 * ((x + 0.5) / CELLS_X - 0.5);
+      var py = 2 * ((y + 0.5) / CELLS_Y - 0.5);
+      var rx = 1 / CELLS_X;
+      var ry = 1 / CELLS_Y;
+      var c = Math.random() / 3;
+      addRect(verts, colors, px, py, 0, rx, ry, c, c, 1 - c);
+    }
+  }
+
+  bgVertBuff = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bgVertBuff);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+  bgColorBuff = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bgColorBuff);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+}
+
+function addRect(verts, colors, px, py, pz, rx, ry, r, g, b) {
+  // Two triangles form a square.
+  verts.push(
+      px-rx, py-ry, pz,
+      px-rx, py+ry, pz,
+      px+rx, py+ry, pz,
+
+      px+rx, py+ry, pz,
+      px+rx, py-ry, pz,
+      px-rx, py-ry, pz);
+  for (var i = 0; i < 6; i++) {
+    colors.push(r, g, b, 1);
+  }
+}
+
+function loop() {
+  maybeResize(canvas, gl);
+  drawScene(gl, program);
+  requestAnimationFrame(loop, canvas);
+}
+
+function maybeResize(canvas, gl) {
+  if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    gl.viewport(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function drawScene(gl, program) {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  var uTranslation = gl.getUniformLocation(program, "uTranslation");
+  var t = Date.now() / 1000;
+//  var translation = [0, 0, 0];
+  var translation = [Math.sin(t), -Math.cos(t), 0];
+  gl.uniform3fv(uTranslation, translation);
+
+  var edgeLength = Math.min(canvas.width, canvas.height);
+  var uScale = gl.getUniformLocation(program, "uScale");
+  var scale = [(Math.sin(t/10) + 1.1) * edgeLength / canvas.width, (Math.sin(t/10) + 1.1) * edgeLength / canvas.height, 1];
+//  var scale = [1 * edgeLength / canvas.width, 1 * edgeLength / canvas.height, 1];
+  gl.uniform3fv(uScale, scale);
+
+  // background
+  drawTriangles(gl, program, bgColorBuff, bgVertBuff, CELLS_COUNT * 2);
+
+  // foreground
+  var verts = [];
+  var colors = [];
+  addRect(verts, colors, Math.sin(t * 5), Math.cos(t * 5), -1, 0.1, 0.1, 1, 1, 1);
+
+  var fgVertBuff = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, fgVertBuff);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+  var fgColorBuff = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, fgColorBuff);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+  drawTriangles(gl, program, fgColorBuff, fgVertBuff, 2);
+}
+
+function drawTriangles(gl, program, colorBuff, vertBuff, triangleCount) {
+  var aVertexColor = gl.getAttribLocation(program, "aVertexColor");
+  gl.enableVertexAttribArray(aVertexColor);
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuff);
+  gl.vertexAttribPointer(aVertexColor, 4, gl.FLOAT, false, 0, 0);
+
+  var aVertexPosition = gl.getAttribLocation(program, "aVertexPosition");
+  gl.enableVertexAttribArray(aVertexPosition);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertBuff);
+  gl.vertexAttribPointer(aVertexPosition, 3, gl.FLOAT, false, 0, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, triangleCount * 3);
+}
