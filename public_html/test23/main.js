a772abf969ac1b596fd3b67e995e05a8b1a362b3
Aaron Whyte <aaron.whyte@gmail.com>
2015-04-11 17:27:11 -0700

    Made test23 look cooler
    and acknowledged that fracas2 is currently broken

--- public_html/test23/main.js
+++ public_html/test23/main.js
@@ -12,9 +12,9 @@ var startMatrix = new Matrix44()
     .multiply(mat4.toTranslateOp(vec4.setXYZ(-2.6, -2, -2)))
     .multiply(mat4.toScaleOp(vec4.setXYZ(0.2, 0.2, 0.2)))
     .multiply(mat4.toRotateZOp(Math.PI / 5))
-    .multiply(mat4.toRotateXOp(0.3));
+    .multiply(mat4.toRotateXOp(0.1));
 var nextCharMatrix = new Matrix44()
-    .multiply(mat4.toTranslateOp(vec4.setXYZ(3.1, 0, 0)))
+    .multiply(mat4.toTranslateOp(vec4.setXYZ(3.1, 0, 0)));
 var ZOOM = 4;
 
 function main() {
@@ -29,13 +29,22 @@ function onRendererLoaded(r) {
 }
 
 function initStamps() {
-  var lineWidth = 0.6;
-  glyphs = new Glyphs(new GlyphMaker(lineWidth, lineWidth));
+  glyphs = new Glyphs(new GlyphMaker(0.7, 5));
   glyphs.initStamps(renderer.gl);
   printer = new Printer(renderer, glyphs.stamps);
 
-  var model = RigidModel.createOctahedron().sphereize(vec4.setXYZ(0, 0, 0), 0.8);
-  stamps.octahedron = model.createModelStamp(renderer.gl);
+  var model = RigidModel.createOctahedron()
+      .createQuadrupleTriangleModel()
+      .createQuadrupleTriangleModel()
+      .sphereize(vec4.setXYZ(0, 0, 0), 0.8);
+  for (var i = 0; i < model.vertexes.length; i++) {
+    var v = model.vertexes[i];
+    var r = Math.random();
+    v.setColorRGB(r * 0.1 + 0.7, r * 0.1 + 0.7, r * 0.1 + 0.7);
+    v.position.scaleToLength(0.4 + Math.random() * 0.8);
+    console.log(v.color);
+  }
+  stamps.asteroid = model.createModelStamp(renderer.gl);
 
   model = RigidModel.createCube().sphereize(vec4.setXYZ(0, 0, 0), 0.7);
   stamps.cube = model.createModelStamp(renderer.gl);
@@ -48,26 +57,22 @@ function loop() {
 
 function drawScene() {
   renderer.resize().clear();
+  var t = Date.now();
 
+  // set view matrix
   var edge = Math.min(canvas.width, canvas.height);
   vec4.setXYZ(edge / (ZOOM * canvas.width), edge / (ZOOM * canvas.height), 0.3);
   viewMatrix.toScaleOp(vec4);
+  viewMatrix.multiply(mat4.toRotateXOp(Math.sin(t / 720) * 0.1));
+  viewMatrix.multiply(mat4.toRotateYOp(Math.sin(t / 650) * 0.1));
 
-  var t = Date.now();
-
-  mat4.toRotateXOp(Math.sin(t / 720) * 0.1);
-  viewMatrix.multiply(mat4);
-
-  mat4.toRotateYOp(Math.sin(t / 650) * 0.1);
-  viewMatrix.multiply(mat4);
-
+  // print text
   renderer.setViewMatrix(viewMatrix);
-
-  renderer.setColorVector(modelColor.setXYZ(Math.random(), Math.random()/5 + 0.5, Math.random()/5 + 0.8));
-
+  renderer.setColorVector(modelColor.setXYZ(Math.random(), Math.random()*0.4 + 0.5, Math.random()*0.2 + 0.8));
   printer.printLine(startMatrix, nextCharMatrix, "CAVE2D.COM!!");
-  var size = 1.0;
 
+  // draw yellow city
+  var size = 1.0;
   renderer
       .setStamp(stamps.cube)
       .setColorVector(modelColor.setXYZ(1, 1, 0.5));
@@ -81,7 +86,7 @@ function drawScene() {
       mat4.toScaleOp(vec4.setXYZ(size, size, size));
       modelMatrix.multiply(mat4);
 
-      mat4.toScaleOp(vec4.setXYZ(1, 1, Math.sin(x * y * t/700)*3.2 + 1));
+      mat4.toScaleOp(vec4.setXYZ(1, 1, Math.sin((x+3) * (y-3) * t/9000)*4 + 1));
       modelMatrix.multiply(mat4);
 
       renderer.setModelMatrix(modelMatrix);
@@ -89,22 +94,23 @@ function drawScene() {
     }
   }
 
-  size = 0.3;
+  // draw asteroids
+  var size = 0.3;
   renderer
-      .setStamp(stamps.octahedron)
+      .setStamp(stamps.asteroid)
       .setColorVector(modelColor.setXYZ(0, 0.8, 1));
   for (var y = -7; y <= 7; y++) {
     for (var x = -7; x <= 7; x++) {
       modelMatrix.toIdentity();
 
-      mat4.toTranslateOp(vec4.setXYZ(x + Math.sin(t/1000), y + Math.cos(t/1000), -2));
+      mat4.toTranslateOp(vec4.setXYZ(
+              x + Math.sin(y/2 + t / 2000),
+              y + Math.cos(x/2 + t / 2000),
+              -1.5));
       modelMatrix.multiply(mat4);
 
-      mat4.toScaleOp(vec4.setXYZ(size, size, size));
-      modelMatrix.multiply(mat4);
-
-      mat4.toRotateZOp(Math.sin((x+y)*(x-y) * t / 900) * 0.1);
-      modelMatrix.multiply(mat4);
+      modelMatrix.multiply(mat4.toRotateZOp(Math.sin((x - y)) * t / 900));
+      modelMatrix.multiply(mat4.toScaleOp(vec4.setXYZ(size, size, size)));
 
       renderer.setModelMatrix(modelMatrix);
       renderer.drawStamp();
