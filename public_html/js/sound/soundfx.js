12b82c3090b4e08d36c2e1e0d8d9cdac2f27bb34
Aaron Whyte <aaron.whyte@gmail.com>
2015-05-02 19:57:17 -0700

    test 25: basic sound
    bouncy-balls make noises now

---
+++ public_html/js/sound/soundfx.js
@@ -0,0 +1,120 @@
+/**
+ * Utils for producing sound effects positioned in 3D.
+ * @param {AudioContext} audioContext
+ * @constructor
+ */
+function SoundFx(audioContext) {
+  this.ctx = audioContext;
+  if (this.ctx) {
+    if (!(this.ctx.createGain || this.ctx.createGainNode) || !this.ctx.createOscillator) {
+      this.ctx = null;
+    }
+  }
+  if (this.ctx) {
+    this.masterGain = this.createGain();
+    this.masterGain.connect(this.ctx.destination);
+  }
+}
+
+SoundFx.audioContext = null;
+
+SoundFx.getAudioContext = function() {
+  if (SoundFx.audioContext != null) {
+    return SoundFx.audioContext;
+  } else if (typeof AudioContext !== 'undefined') {
+    SoundFx.audioContext = new AudioContext();
+  } else if (typeof webkitAudioContext !== 'undefined') {
+    SoundFx.audioContext = new webkitAudioContext();
+  }
+  return SoundFx.audioContext;
+};
+
+SoundFx.prototype.createGain = function() {
+  if (this.ctx.createGain) {
+    return this.ctx.createGain();
+  }
+  if (this.ctx.createGainNode) {
+    return this.ctx.createGainNode();
+  }
+  return null;
+};
+
+SoundFx.prototype.setListenerXYZ = function(x, y, z) {
+  if (!this.ctx) return;
+  this.ctx.listener.setPosition(x, y, z);
+};
+
+SoundFx.prototype.getMasterGain = function() {
+  return this.masterGain;
+};
+
+/**
+ * Make a simple one-shot sound.
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @param {number} vol
+ * @param {number} attack
+ * @param {number} decay
+ * @param {number} freq1
+ * @param {number} freq2
+ * @param {String} type Wave type string (square, sine, etc)
+ */
+SoundFx.prototype.sound = function(x, y, z, vol, attack, sustain, decay, freq1, freq2, type, opt_delay) {
+  if (!this.ctx) return;
+  var delay = opt_delay || 0;
+  var c = this.ctx;
+  var t0 = c.currentTime + delay;
+  var t1 = t0 + attack + sustain + decay;
+  var gain = this.createGain();
+  gain.gain.value = 0;
+  gain.gain.setValueAtTime(0, t0);
+  gain.gain.linearRampToValueAtTime(vol, t0 + attack);
+  gain.gain.linearRampToValueAtTime(0, t0 + attack + decay);
+
+  var osc = c.createOscillator();
+  osc.frequency.setValueAtTime(freq1, t0);
+  osc.frequency.exponentialRampToValueAtTime(freq2, t0 + attack + decay);
+  osc.type = type;
+  if (osc.start) {
+    osc.start(t0);
+  } else if (osc.noteOn) {
+    osc.noteOn(t0);
+  }
+  if (osc.stop) {
+    osc.stop(t1);
+  } else if (osc.noteOff) {
+    osc.noteOff(t1);
+  }
+
+  var panner = c.createPanner();
+  panner.setPosition(x, y, z);
+
+  osc.connect(gain);
+  gain.connect(panner);
+  panner.connect(this.masterGain);
+};
+
+/**
+ * Makes a sound with no attack, decay, or frequency changes.
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @param {number} vol
+ * @param {number} sustain
+ * @param {number} freq
+ * @param {String} opt_type Wave type string (square, sine, etc). Default is sine
+ */
+SoundFx.prototype.note = function(x, y, z, vol, sustain, freq, opt_type) {
+  var type = opt_type || 'sine';
+  this.sound(x, y, z, vol, 0, sustain, 0, freq, freq, type);
+}
+
+SoundFx.prototype.disconnect = function() {
+  if (this.masterGain) {
+    this.masterGain.gain = 0;
+    this.masterGain.disconnect();
+    this.masterGain = null;
+  }
+};
+
---
+++ public_html/test25/main.js
@@ -0,0 +1,300 @@
+var canvas, renderer;
+
+var vec4 = new Vec4();
+var mat4 = new Matrix44();
+var array3 = [0, 0, 0];
+var bodyPos = new Vec2d();
+var strikeVec = new Vec2d();
+
+var viewMatrix = new Matrix44();
+var modelMatrix = new Matrix44();
+var screenToClipMatrix = new Matrix44();
+var screenToWorldMatrix = new Matrix44();
+var modelColor = new Vec4();
+
+var stamps = {};
+
+var ZOOM = 15;
+var MS_PER_FRAME = 1000 / 60;
+var CLOCKS_PER_FRAME = 1;
+var PATH_DURATION = Math.max(10, CLOCKS_PER_FRAME);
+var lastPathRefreshTime = 0;
+
+var FLOOR_RAD = 7;
+var MAX_CIRCLES = 4;
+
+var world, resolver;
+
+var pointer, pScreenVec4, pWorldVec4;
+var POINTER_RAD = 0.5;
+var POINTER_HEIGHT = 2.1;
+
+var sound;
+
+function main() {
+  canvas = document.querySelector('#canvas');
+  new RendererLoader(canvas, 'vertex-shader.txt', 'fragment-shader.txt').load(onRendererLoaded);
+  pScreenVec4 = new Vec4();
+  pWorldVec4 = new Vec4();
+  pointer = new MonoPointer();
+  pointer.startListening();
+  sound = new SoundFx(SoundFx.getAudioContext());
+}
+
+function onRendererLoaded(r) {
+  renderer = r;
+  initStamps();
+  initWorld();
+  loop();
+}
+
+function initStamps() {
+  var glyphStamps = new Glyphs(new GlyphMaker(0.6, 2)).initStamps(renderer.gl);
+  for (var key in glyphStamps) {
+    stamps[key] = glyphStamps[key];
+  }
+  stamps.sphere = RigidModel.createOctahedron()
+      .createQuadrupleTriangleModel()
+      .createQuadrupleTriangleModel()
+      .createQuadrupleTriangleModel()
+      .sphereize(vec4.setXYZ(0, 0, 0), 1)
+      .createModelStamp(renderer.gl);
+  stamps.cube = RigidModel.createCube().createModelStamp(renderer.gl);
+
+  var canModel = RigidModel.createOctahedron()
+      .createQuadrupleTriangleModel()
+      .createQuadrupleTriangleModel()
+      .createQuadrupleTriangleModel()
+      .sphereize(vec4.setXYZ(0, 0, 0), 3);
+  for (var i = 0; i < canModel.vertexes.length; i++) {
+    var v = canModel.vertexes[i].position.v;
+    if (v[2] > 1) v[2] = 1;
+    if (v[2] < -1) v[2] = -1;
+    var mag = Vec2d.magnitude(v[0], v[1]);
+    if (mag > 1) {
+      v[0] = v[0] / mag;
+      v[1] = v[1] / mag;
+    }
+  }
+  stamps.can = canModel.createModelStamp(renderer.gl);
+}
+
+function initWorld() {
+  world = new World();
+  resolver = new HitResolver();
+  var v = new Vec2d();
+
+  var circles = 0;
+  for (var y = -FLOOR_RAD; y <= FLOOR_RAD; y++) {
+    for (var x = -FLOOR_RAD ; x <= FLOOR_RAD; x++) {
+      var b = Body.alloc();
+      v.setXY(x * 2, y * 2);
+      b.setPosAtTime(v, 1);
+      b.group = 0;
+      var rand = Math.random();
+      // Stationary wall
+      if (Math.abs(y) == FLOOR_RAD || Math.abs(x) == FLOOR_RAD) {
+        b.shape = Body.Shape.RECT;
+        b.mass = Infinity;
+        b.pathDurationMax = Infinity;
+        b.rectRad.setXY(1, 1);
+        world.addBody(b);
+      } else if (Math.random() < 0.1) {
+        b.shape = Body.Shape.RECT;
+        b.mass = Infinity;
+        b.pathDurationMax = Infinity;
+        b.rectRad.setXY(0.5 + Math.random(), 0.5 + Math.random());
+        world.addBody(b);
+      } else if (Math.random() < 0.1 && circles < MAX_CIRCLES) {
+        circles++;
+        b.shape = Body.Shape.CIRCLE;
+        b.rad = 1 - Math.random() * 0.5;
+        b.mass = 4/3 * Math.PI * Math.pow(b.rad, 3);
+        b.setVelXYAtTime(Math.random() - 0.5, Math.random() - 0.5, world.now);
+        b.pathDurationMax = PATH_DURATION;
+        world.addBody(b);
+      }
+    }
+  }
+}
+
+function loop() {
+  clock();
+  drawScene();
+  requestAnimationFrame(loop, canvas);
+}
+
+function clock() {
+  var endTimeMs = Date.now() + MS_PER_FRAME;
+  var endClock = world.now + CLOCKS_PER_FRAME;
+  var e = world.getNextEvent();
+  // Stop if there are no more events to process, or we've moved the game clock far enough ahead
+  // to match the amount of wall-time elapsed since the last frame,
+  // or (worst case) we're out of time for this frame.
+
+  while (e && e.time <= endClock && Date.now() <= endTimeMs) {
+    if (e.type == WorldEvent.TYPE_HIT) {
+      var b0 = world.getBodyByPathId(e.pathId0);
+      var b1 = world.getBodyByPathId(e.pathId1);
+      if (b0 && b1) {
+        resolver.resolveHit(e.time, e.collisionVec, b0, b1);
+
+        strikeVec.set(b1.vel).subtract(b0.vel).projectOnto(e.collisionVec);
+        var mag = strikeVec.magnitude();
+
+        bonk(b0, mag + 0.5);
+        bonk(b1, mag + 0.5);
+      }
+    }
+    world.processNextEvent();
+    e = world.getNextEvent();
+  }
+  if (!e || e.time > endClock) {
+    world.now = endClock;
+  }
+
+  if (lastPathRefreshTime + PATH_DURATION - CLOCKS_PER_FRAME <= world.now) {
+    lastPathRefreshTime = world.now;
+    for (var id in world.bodies) {
+      var b = world.bodies[id];
+      if (b && b.shape === Body.Shape.CIRCLE) {
+        b.invalidatePath();
+      }
+    }
+  }
+}
+
+function bonk(b, mag) {
+  var vol = mag * 0.5;
+  b.getPosAtTime(world.now, bodyPos);
+  vec4.setXYZ(bodyPos.x, bodyPos.y, 0);
+  vec4.transform(viewMatrix);
+  if (b.shape == Body.Shape.RECT) {
+    var mass = 1 * b.rectRad.x * b.rectRad.y;
+    for (var i = 0; i < 6; i++) {
+      var freq = (0.5 * (Math.random() + 0.5)) * 4000/mass;
+      var dur = 20 /(freq + 500);
+      sound.sound(vec4.v[0], vec4.v[1], vec4.v[2], vol, 0, 0, dur, freq, freq, 'sine');
+    }
+  } else {
+    var mass = 3 * Math.PI * Math.pow(b.rad, 3);
+    for (var i = 0; i < 6; i++) {
+      var freq = 200 + (0.1 * (Math.random() + 0.5)) * 4000/mass;
+      var dur = 30 /(freq + 500);
+      sound.sound(vec4.v[0], vec4.v[1], vec4.v[2], vol, 0, 0, dur, freq, freq, 'sine');
+    }
+  }
+
+}
+
+function isPointing() {
+  return true;
+}
+
+function drawScene() {
+  renderer.resize().clear();
+  var t = Date.now();
+  setViewMatrix(t);
+  setPointerWorldVec();
+
+  // draw pointer puck
+  if (isPointing()) {
+    renderer
+        .setStamp(stamps.can)
+        .setColorVector(modelColor.setXYZ(
+                0.5 + 0.25 * Math.sin(t / 60),
+                0.5 + 0.25 * Math.sin(2*Math.PI/3 + t / 60),
+                0.5 + 0.25 * Math.sin(4*Math.PI/3 + t / 60)));
+    modelMatrix.toTranslateOp(pWorldVec4);
+    modelMatrix.multiply(mat4.toScaleOp(vec4.setXYZ(POINTER_RAD, POINTER_RAD, POINTER_HEIGHT*0.5)));
+    renderer.setModelMatrix(modelMatrix);
+    renderer.drawStamp();
+  }
+
+  // walls
+  // If I was serious, the static wall vertexes would be loaded into GL memory ahead of time.
+  renderer
+      .setStamp(stamps.cube)
+      .setColorVector(modelColor.setXYZ(0.7, 0.7, 0.7));
+  for (var id in world.bodies) {
+    var b = world.bodies[id];
+    if (b && b.shape === Body.Shape.RECT) {
+      b.getPosAtTime(world.now, bodyPos);
+      if (isPointing() &&
+          Math.abs(pWorldVec4.v[0] - bodyPos.x) <= b.rectRad.x + POINTER_RAD &&
+          Math.abs(pWorldVec4.v[1] - bodyPos.y) <= b.rectRad.y + POINTER_RAD) {
+        renderer.setColorVector(modelColor.setXYZ(1, 0, 0));
+        drawBody(b);
+        renderer.setColorVector(modelColor.setXYZ(0.7, 0.7, 0.7));
+      } else {
+        drawBody(b);
+      }
+    }
+  }
+
+  // draw spheres
+  renderer
+      .setStamp(stamps.sphere)
+      .setColorVector(modelColor.setXYZ(0, 1, 0));
+  for (var id in world.bodies) {
+    var b = world.bodies[id];
+    if (b && b.shape === Body.Shape.CIRCLE) {
+      b.getPosAtTime(world.now, bodyPos);
+      if (isPointing() &&
+          Vec2d.magnitude(pWorldVec4.v[0] - bodyPos.x, pWorldVec4.v[1] - bodyPos.y) <= b.rad + POINTER_RAD) {
+        renderer.setColorVector(modelColor.setXYZ(1, 0, 0));
+        drawBody(b);
+        renderer.setColorVector(modelColor.setXYZ(0, 1, 0));
+      } else {
+        drawBody(b);
+      }
+    }
+  }
+}
+
+function setViewMatrix(t) {
+  // set view matrix
+  var edge = Math.min(canvas.width, canvas.height / (Math.sqrt(2)/2));
+  viewMatrix.toIdentity();
+
+  viewMatrix.multiply(mat4.toScaleOp(vec4.setXYZ(
+          edge / (ZOOM * canvas.width),
+          Math.sqrt(2)/2 * edge / (ZOOM * canvas.height),
+      0.5)));
+
+  // Shear
+  mat4.toIdentity();
+  mat4.setColRowVal(2, 1, -1.1);
+  viewMatrix.multiply(mat4);
+
+  // Slow spin
+  viewMatrix.multiply(mat4.toRotateZOp(Math.PI * t / 20000));
+  renderer.setViewMatrix(viewMatrix);
+}
+
+function setPointerWorldVec() {
+  pScreenVec4.setXYZ(pointer.pos.x, pointer.pos.y, 0);
+
+  screenToClipMatrix.toIdentity();
+  screenToClipMatrix.multiply(mat4.toScaleOp(vec4.setXYZ(2 / canvas.width, -2 / canvas.height, 0)));
+  screenToClipMatrix.multiply(mat4.toTranslateOp(vec4.setXYZ(-canvas.width / 2, -canvas.height / 2, 0)));
+
+  pWorldVec4.set(pScreenVec4).transform(screenToClipMatrix);
+
+  viewMatrix.getInverse(mat4);
+  pWorldVec4.transform(mat4);
+}
+
+function drawBody(b) {
+  b.getPosAtTime(world.now, bodyPos);
+  if (b.shape == Body.Shape.RECT) {
+    modelMatrix.toTranslateOp(vec4.setXYZ(bodyPos.x, bodyPos.y, 0));
+    modelMatrix.multiply(mat4.toScaleOp(vec4.setXYZ(b.rectRad.x, b.rectRad.y, 1)));
+  } else {
+    modelMatrix.toTranslateOp(vec4.setXYZ(bodyPos.x, bodyPos.y, 0));
+    modelMatrix.multiply(mat4.toScaleOp(vec4.setXYZ(b.rad, b.rad, b.rad)));
+  }
+  renderer.setModelMatrix(modelMatrix);
+  renderer.drawStamp();
+}
+
