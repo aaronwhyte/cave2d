96b7359fdad679f94042aa613531380f36ff1794
Aaron Whyte <aaron.whyte@gmail.com>
2015-09-03 09:08:33 -0700

    Fixed quadteegrid-test

--- public_html/js/quads/painter.js
+++ public_html/js/quads/painter.js
@@ -9,6 +9,8 @@ function Painter() {
  * @param cy center of the square
  * @param radius radius of the square (half height and half width)
  * @param maxed whether this square is at the maximum quadtree depth or not.
+ * @param oldColor The color of the area being painted, which is either an integer if the area is uniform, or
+ * an array of four elements if it is a mix of four children.
  * @return PAINT_NOTHING, PAINT_DETAILS (only on a non-maxed square), or a paint color
  */
 Painter.prototype.getEffect = function(cx, cy, radius, maxed, oldColor) {
