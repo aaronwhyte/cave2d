1862ad9ba7158198925110d6d3586492b45f216d
Aaron Whyte <aaron.whyte@gmail.com>
2016-01-03 00:02:03 -0800

    Added MousePointerTrigger
    Like TouchTrigger, but for the mouse pointer.

---
+++ public_html/js/controls/mousepointertrigger.js
@@ -0,0 +1,68 @@
+/**
+ * A control Trigger using a mouse pointer.
+ * @constructor
+ * @extends {Trigger}
+ */
+function MousePointerTrigger(opt_elem) {
+  Trigger.call(this);
+  this.listening = false;
+  this.elem = opt_elem || document.body;
+
+  this.startZoneFn = function(x, y) {
+    return true;
+  };
+
+  var self = this;
+  this.mouseDownListener = function(e) {
+    return self.onMouseDown(e);
+  };
+  this.mouseUpListener = function(e) {
+    return self.onMouseUp(e);
+  };
+}
+
+MousePointerTrigger.prototype = new Trigger();
+MousePointerTrigger.prototype.constructor = MousePointerTrigger;
+
+/**
+ * @param {function} fn  A function that takes screen coords (x, y) and returns true if the coords are
+ * within the trigger start zone.
+ * @returns {MousePointerTrigger}
+ */
+MousePointerTrigger.prototype.setStartZoneFunction = function(fn) {
+  this.startZoneFn = fn;
+  return this;
+};
+
+MousePointerTrigger.prototype.startListening = function() {
+  this.elem.addEventListener('mousedown', this.mouseDownListener);
+  this.elem.addEventListener('mouseup', this.mouseUpListener);
+  return this;
+};
+
+MousePointerTrigger.prototype.stopListening = function() {
+  this.elem.removeEventListener('mousedown', this.mouseDownListener);
+  this.elem.removeEventListener('mouseup', this.mouseUpListener);
+  this.val = false;
+  return this;
+};
+
+MousePointerTrigger.prototype.onMouseDown = function(e) {
+  if (this.startZoneFn(e.pageX, e.pageY)) {
+    this.val = true;
+    this.publishTriggerDown();
+
+    // For LayeredEventDistributor
+    return false;
+  }
+};
+
+MousePointerTrigger.prototype.onMouseUp = function(e) {
+  if (this.val) {
+    this.val = false;
+    this.publishTriggerUp();
+
+    // For LayeredEventDistributor
+    return false;
+  }
+};
