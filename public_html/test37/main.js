65c9999b8bd5fa73a00091263ec2932523f2a512
Aaron Whyte <aaron.whyte@gmail.com>
2016-01-03 22:02:35 -0800

    more iOS audio unlock, and changed test36 to test37
    If I start with a drag, the iOS Safari audio unlocker doesn’t work. It
    needs to be a clean touch. For now, I’ll unlock on every touchend,
    since it is inaudible and low cost. Grrr.

--- public_html/test37/main.js
+++ public_html/test37/main.js
@@ -1,58 +1,59 @@
-var test36;
+var test37;
 function main() {
-  test36 = new Test36(document.querySelector('#canvas'));
-  test36.start();
+  test37 = new Test37(document.querySelector('#canvas'));
+  test37.start();
 }
 
-function Test36(canvas) {
+function Test37(canvas) {
   this.canvas = canvas;
-  this.iosSoundUnlocked = false;
+  this.iosSoundUnlocked = 0;
   this.animateFrameFn = this.animateFrame.bind(this);
 }
 
-Test36.prototype.start = function() {
+Test37.prototype.start = function() {
   new RendererLoader(this.canvas, 'vertex-shader.txt', 'fragment-shader.txt')
       .load(this.onRendererLoaded.bind(this));
   this.sfx = new SoundFx();
   this.sfx.setListenerXYZ(0, 0, 5);
 
-  // on-event sound unlocker for iOS
-  this.canvas.addEventListener('mouseup', this.unlockIosSound.bind(this));
+  // On-event sound unlocker for iOS.
   this.canvas.addEventListener('touchend', this.unlockIosSound.bind(this));
 };
 
-Test36.SCREEN_PLAY = 'play';
+Test37.SCREEN_PLAY = 'play';
 
-Test36.SCREENS = [Test36.SCREEN_PLAY];
+Test37.SCREENS = [Test37.SCREEN_PLAY];
 
-Test36.prototype.unlockIosSound = function() {
-  if (!this.iosSoundUnlocked) {
-    this.sfx.sound(0, 0, 0, 0.001, 0, 0, 0.001, 1, 1, 'sine');
-    this.iosSoundUnlocked = true;
-  }
+/**
+ * It seems that a drag won't work. There has to be a clean tap.
+ * For now, I'll unlock every time there's a touchend.
+ */
+Test37.prototype.unlockIosSound = function() {
+  this.sfx.sound(0, 0, 0, 0.001, 0, 0, 0.001, 1, 1, 'sine');
+  this.iosSoundUnlocked++;
 };
 
-Test36.prototype.onRendererLoaded = function(r) {
+Test37.prototype.onRendererLoaded = function(r) {
   this.renderer = r;
   this.initScreens();
   this.requestAnimation();
 };
 
-Test36.prototype.initScreens = function() {
+Test37.prototype.initScreens = function() {
   this.initStamps();
   this.screens = {};
-  this.screens[Test36.SCREEN_PLAY] = new PlayScreen(this, this.canvas, this.renderer, this.glyphs, this.stamps, this.sfx);
+  this.screens[Test37.SCREEN_PLAY] = new PlayScreen(this, this.canvas, this.renderer, this.glyphs, this.stamps, this.sfx);
 
   this.visibility = {};
-  for (var i = 0; i < Test36.SCREENS.length; i++) {
-    var screen = Test36.SCREENS[i];
-    this.visibility[screen] = screen == Test36.SCREEN_PLAY ? 1 : 0;
+  for (var i = 0; i < Test37.SCREENS.length; i++) {
+    var screen = Test37.SCREENS[i];
+    this.visibility[screen] = screen == Test37.SCREEN_PLAY ? 1 : 0;
   }
-  this.frontScreenId = Test36.SCREEN_PLAY;
+  this.frontScreenId = Test37.SCREEN_PLAY;
   this.animationRequested = false;
 };
 
-Test36.prototype.initStamps = function() {
+Test37.prototype.initStamps = function() {
   var glyphMaker = new GlyphMaker(0.4, 1.2);
   this.glyphs = new Glyphs(glyphMaker);
   var glyphStamps = this.glyphs.initStamps(this.renderer.gl);
@@ -65,11 +66,11 @@ Test36.prototype.initStamps = function() {
 /**
  * Manages changing screen visibility, and calls drawScreen() on visible screens.
  */
-Test36.prototype.animateFrame = function() {
+Test37.prototype.animateFrame = function() {
   this.animationRequested = false;
   this.renderer.resize().clear();
-  for (var i = 0; i < Test36.SCREENS.length; i++) {
-    var id = Test36.SCREENS[i];
+  for (var i = 0; i < Test37.SCREENS.length; i++) {
+    var id = Test37.SCREENS[i];
     var oldVisibility = this.visibility[id];
     var seconds = 0.2;
     if (this.frontScreenId == id) {
@@ -87,17 +88,17 @@ Test36.prototype.animateFrame = function() {
   }
 };
 
-Test36.prototype.gotoScreen = function(screenId) {
+Test37.prototype.gotoScreen = function(screenId) {
   this.frontScreenId = screenId;
   this.requestAnimation();
 };
 
-Test36.prototype.restart = function() {
-  this.screens[Test36.SCREEN_PLAY].unloadLevel();
+Test37.prototype.restart = function() {
+  this.screens[Test37.SCREEN_PLAY].unloadLevel();
   this.requestAnimation();
 };
 
-Test36.prototype.requestFullScreen = function() {
+Test37.prototype.requestFullScreen = function() {
   var elem = document.body;
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
@@ -111,7 +112,7 @@ Test36.prototype.requestFullScreen = function() {
   this.requestAnimation();
 };
 
-Test36.prototype.requestPointerLock = function() {
+Test37.prototype.requestPointerLock = function() {
   this.canvas.requestPointerLock = this.canvas.requestPointerLock ||
       this.canvas.mozRequestPointerLock ||
       this.canvas.webkitRequestPointerLock;
@@ -120,7 +121,7 @@ Test36.prototype.requestPointerLock = function() {
   }
 };
 
-Test36.prototype.exitPointerLock = function() {
+Test37.prototype.exitPointerLock = function() {
   document.exitPointerLock = document.exitPointerLock ||
       document.mozExitPointerLock ||
       document.webkitExitPointerLock;
@@ -131,7 +132,7 @@ Test36.prototype.exitPointerLock = function() {
   }
 };
 
-Test36.prototype.requestAnimation = function() {
+Test37.prototype.requestAnimation = function() {
   if (!this.animationRequested) {
     this.animationRequested = true;
     requestAnimationFrame(this.animateFrameFn, this.canvas);
