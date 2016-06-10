2e56b6918b347e30abd04cdd110257ea7fa3c817
Aaron Whyte <aaron.whyte@gmail.com>
2016-06-09 22:47:42 -0700

    game2 10% faster ants

--- public_html/game2/spirits/antspirit.js
+++ public_html/game2/spirits/antspirit.js
@@ -32,7 +32,7 @@ AntSpirit.prototype = new BaseSpirit();
 AntSpirit.prototype.constructor = AntSpirit;
 
 AntSpirit.MEASURE_TIMEOUT = 1.2;
-AntSpirit.THRUST = 0.3;
+AntSpirit.THRUST = 0.33;
 AntSpirit.MAX_TIMEOUT = 10;
 AntSpirit.LOW_POWER_VIEWPORTS_AWAY = 2;
 AntSpirit.STOPPING_SPEED_SQUARED = 0.01 * 0.01;
