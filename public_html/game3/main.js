37ada9f94ba2e9ea60dbcb58c7d9803010c85b43
Aaron Whyte <aaron.whyte@gmail.com>
2016-10-02 18:53:17 -0700

    renamed file to break caching

--- public_html/game3/main.js
+++ public_html/game3/main.js
@@ -2,7 +2,7 @@ var app;
 function main() {
   var basePath = ['game3'];
   app = new PlayApp('Game 3', basePath,
-      'adventures/demo1.txt',
+      'adventures/demo2.txt',
       'vertex-shader.txt', 'fragment-shader.txt',
       PlayLevelPage);
   app.start();
