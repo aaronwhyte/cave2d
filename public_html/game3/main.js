f562ed24b4c96a93e0424a5fddc551a01cf02361
Aaron Whyte <aaron.whyte@gmail.com>
2016-10-05 00:06:05 -0700

    game3 demo map change
    added some other maps (unreachable for now) with finer bitgrid squares,
    down to 1/5th of the original.

--- public_html/game3/main.js
+++ public_html/game3/main.js
@@ -2,7 +2,7 @@ var app;
 function main() {
   var basePath = ['game3'];
   app = new PlayApp('Game 3', basePath,
-      'adventures/demo2.txt',
+      'adventures/demo3.txt',
       'vertex-shader.txt', 'fragment-shader.txt',
       PlayLevelPage);
   app.start();
