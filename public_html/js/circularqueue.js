abc6d41aef3b1c7cb7ca64e5f6393e9a195cbc71
Aaron Whyte <aaron.whyte@gmail.com>
2016-12-03 21:56:55 -0800

    CircularQueue pop
    and modified unit test from plexode

--- public_html/js/circularqueue.js
+++ public_html/js/circularqueue.js
@@ -37,7 +37,7 @@ CircularQueue.prototype.isFull = function() {
 };
 
 /**
- * Adds an item to the head of the queue.
+ * Adds (pushes) an item to the head of the queue.
  * If the queue is full, an item is dropped from the tail.
  * @param val  Any value, to be enqueued.
  * @return the object that fell off the tail, or null if nothing fell off
@@ -83,6 +83,30 @@ CircularQueue.prototype.dequeue = function() {
     this.tail++;
     if (this.tail >= this.maxLen) {
       this.tail = 0;
+    }
+  }
+  return val;
+};
+
+
+/**
+ * @return Whatever was pulled off the head of the queue, or null if the queue is empty. This is just like dequeue,
+ * except that it removes from the other end, so "pop" and "enqueue" act like a stack.
+ */
+CircularQueue.prototype.pop = function() {
+  if (this.head == -1) {
+    // empty
+    return null;
+  }
+  var val = this.a[this.head];
+  if (this.tail == this.head) {
+    // now it's empty
+    this.head = this.tail = -1;
+  } else {
+    // move head back
+    this.head--;
+    if (this.head < 0) {
+      this.head = this.maxLen - 1;
     }
   }
   return val;
