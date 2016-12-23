e6dd905b5b3ab0eeb7b4068fa9c984ade1ae10ba
Aaron Whyte <aaron.whyte@gmail.com>
2016-12-22 22:59:51 -0800

    test41 zoom back in a bit

--- public_html/test41/basescreen.js
+++ public_html/test41/basescreen.js
@@ -225,7 +225,7 @@ BaseScreen.prototype = new Screen();
 BaseScreen.prototype.constructor = BaseScreen;
 
 BaseScreen.WIDGET_RADIUS = 30;
-BaseScreen.CAMERA_VIEW_DIST = 40;
+BaseScreen.CAMERA_VIEW_DIST = 35;
 
 BaseScreen.MS_PER_FRAME = 1000 / 60;
 BaseScreen.MS_UNTIL_CLOCK_ABORT = BaseScreen.MS_PER_FRAME - 1;
