323fc4ebe61baf47909457579bbf0e6c25e61ba3
Aaron Whyte <aaron.whyte@gmail.com>
2017-01-10 22:40:28 -0800

    de-duped game2 page code
    Pulled a bunch of duplicate game2 page code up to new ScreenPage
    baseclass.

--- public_html/game2/edit/testlevelpage.js
+++ public_html/game2/edit/testlevelpage.js
@@ -9,96 +9,12 @@
  * @extends (Page)
  */
 function TestLevelPage(gameTitle, basePath, fileTree, adventureName, levelName) {
-  Page.call(this);
-  this.gameTitle = gameTitle;
-  this.basePath = basePath;
-  this.fileTree = fileTree;
-  this.adventureName = adventureName;
-  this.levelName = levelName;
-  this.levelDataPath = BaseApp.path(this.basePath, this.adventureName, this.levelName)
-      .concat(BaseApp.PATH_LEVEL_JSON);
-
-  this.canvas = null;
-  this.pauseMenuDiv = null;
-
-  this.oldMetaViewportContent = null;
-
-  this.animateFrameFn = this.animateFrame.bind(this);
+  ScreenPage.call(this, null, gameTitle, basePath, fileTree, adventureName, levelName);
 }
-TestLevelPage.prototype = new Page();
+TestLevelPage.prototype = new ScreenPage();
 TestLevelPage.prototype.constructor = TestLevelPage;
 
-TestLevelPage.prototype.enterDoc = function() {
-  if (this.canvas || this.pauseMenuDiv) {
-    throw Error('nodes should be falsey. canvas:' + this.canvas + 'pauseMenuDiv:' + this.pauseMenuDiv);
-  }
-  var df = document.createDocumentFragment();
-
-  this.canvas = Dom.ce('canvas', df);
-  this.canvas.id = 'canvas';
-
-  this.pauseMenuDiv = Dom.ce('div', df);
-  this.pauseMenuDiv.id = 'pauseMenu';
-  document.body.appendChild(df);
-
-  document.body.classList.add('canvasPage');
-
-  var metaViewport = document.head.querySelector('meta[name="viewport"]');
-  this.oldMetaViewportContent = metaViewport.content;
-  metaViewport.content = 'width=device-width, user-scalable=no';
-
-  this.refreshPauseMenu();
-
-  this.sfx = new SoundFx();
-  this.sfx.setListenerXYZ(0, 0, 2);
-
-  // On-event sound unlocker for iOS.
-  var resumeButton = document.querySelector('#resumeButton');
-  var boundUnlock = this.unlockIosSound.bind(this);
-  resumeButton.addEventListener('touchend', boundUnlock);
-  resumeButton.addEventListener('touchstart', boundUnlock);
-  this.canvas.addEventListener('touchend', boundUnlock);
-  this.canvas.addEventListener('touchstart', boundUnlock);
-
-  this.canvas.addEventListener('touchstart', Dom.pd);
-  this.canvas.addEventListener('touchmove', Dom.pd);
-  this.canvas.addEventListener('touchend', Dom.pd);
-
-  window.addEventListener("scroll", Dom.pd);
-
-  // load level
-  this.jsonObj = this.fileTree.getFile(this.levelDataPath);
-};
-
-/**
- * It seems that a drag won't work. There has to be a clean tap.
- * For now, I'll unlock every time there's a touchend.
- */
-TestLevelPage.prototype.unlockIosSound = function() {
-  this.sfx.sound(0, 0, 0, 0.001, 0, 0, 0.001, 1, 1, 'sine');
-  this.iosSoundUnlocked++;
-};
-
-TestLevelPage.prototype.exitDoc = function() {
-  if (!this.canvas || !this.pauseMenuDiv) {
-    throw Error('nodes should be truthy. canvas:' + this.canvas + 'pauseMenuDiv:' + this.pauseMenuDiv);
-  }
-  window.removeEventListener("scroll", Dom.pd);
-
-  if (this.screen) {
-    this.screen.setScreenListening(false);
-  }
-  document.body.removeChild(this.canvas);
-  document.body.removeChild(this.pauseMenuDiv);
-  document.body.classList.remove('canvasPage');
-  this.canvas = null;
-  this.pauseMenuDiv = null;
-  this.animationId = 0;
-
-  var metaViewport = document.head.querySelector('meta[name="viewport"]');
-  metaViewport.content = this.oldMetaViewportContent;
-  this.oldMetaViewportContent = null;
-};
+TestLevelPage.prototype.maybeSaveLevel = function() {};
 
 TestLevelPage.prototype.refreshPauseMenu = function() {
   var df = document.createDocumentFragment();
@@ -128,37 +44,14 @@ TestLevelPage.prototype.refreshPauseMenu = function() {
   e = Dom.ce('span', label);
   e.innerHTML = Strings.textToHtml(' draw rayscans');
 
-  e = Dom.ce('button', df, 'smallButton');
-  e.id = 'fullScreenButton';
-  e.innerHTML = Strings.textToHtml('full screen');
-
+  this.appendFullScreenButton(df);
   Dom.ce('br', df);
+  this.appendResumeButton(df);
 
-  e = Dom.ce('button', df, 'mainButton');
-  e.id = 'resumeButton';
-  e.innerHTML = Strings.textToHtml('resume');
-
-  this.pauseMenuDiv.innerHTML = '';
-  this.pauseMenuDiv.appendChild(df);
+  this.setPauseMenuContent(df);
 };
 
-TestLevelPage.prototype.onShaderTextChange = function(vertexShaderText, fragmentShaderText) {
-  if (!this.canvas) {
-    console.log('onShaderTextChange with no this.canvas');
-    return;
-  }
-
-  var gl = getWebGlContext(this.canvas, {
-    alpha: false,
-    antialias: true
-  });
-  var vs = compileShader(gl, vertexShaderText, gl.VERTEX_SHADER);
-  var fs = compileShader(gl, fragmentShaderText, gl.FRAGMENT_SHADER);
-  var program = createProgram(gl, vs, fs);
-  gl.enable(gl.DEPTH_TEST);
-  gl.useProgram(program);
-  this.renderer = new Renderer(this.canvas, gl, program);
-
+TestLevelPage.prototype.maybeCreateScreen = function() {
   this.screen = new Game2TestScreen(
       this, this.canvas, this.renderer, Stamps.create(this.renderer), this.sfx, this.adventureName, this.levelName);
   this.screen.initWidgets();
@@ -170,31 +63,5 @@ TestLevelPage.prototype.onShaderTextChange = function(vertexShaderText, fragment
     this.screen.createDefaultWorld();
   }
 
-  this.requestAnimation();
-};
-
-TestLevelPage.prototype.requestAnimation = function() {
-  if (!this.animationId && this.canvas) {
-    this.animationId = requestAnimationFrame(this.animateFrameFn, this.canvas);
-  }
-};
-
-TestLevelPage.prototype.animateFrame = function(startTimeMs) {
-  if (!this.animationId) {
-    return;
-  }
-  if (!this.canvas) {
-    console.log('animateFrame with no this.canvas');
-    return;
-  }
-
-  this.animationId = 0;
-  this.renderer.resize().clear();
-  this.screen.setScreenListening(true);
-  this.screen.drawScreen(1, startTimeMs);
-};
-
-TestLevelPage.prototype.requestFullScreen = function() {
-  Dom.requestFullScreen();
   this.requestAnimation();
 };
---
+++ public_html/js/app/screenpage.js
@@ -0,0 +1,195 @@
+/**
+ * WebGL page baseclass
+ * @param {BaseApp} app
+ * @param {String} gameTitle
+ * @param {Array.<String>} basePath of the game
+ * @param {FileTree} fileTree
+ * @param {String} adventureName
+ * @param {String} levelName
+ * @constructor
+ * @extends (Page)
+ */
+function ScreenPage(app, gameTitle, basePath, fileTree, adventureName, levelName) {
+  Page.call(this);
+  if (gameTitle == null) {
+    // probably making a prototype
+    return;
+  }
+  this.app = app;
+  this.gameTitle = gameTitle;
+  this.basePath = basePath;
+  this.fileTree = fileTree;
+  this.adventureName = adventureName;
+  this.levelName = levelName;
+  this.levelDataPath = BaseApp.path(this.basePath, this.adventureName, this.levelName)
+      .concat(BaseApp.PATH_LEVEL_JSON);
+
+  this.canvas = null;
+  this.pauseMenuDiv = null;
+  this.oldMetaViewportContent = null;
+  this.animateFrameFn = this.animateFrame.bind(this);
+  this.paused = false;
+}
+ScreenPage.prototype = new Page();
+ScreenPage.prototype.constructor = ScreenPage;
+
+ScreenPage.prototype.enterDoc = function() {
+  if (this.canvas || this.pauseMenuDiv) {
+    throw Error('nodes should be falsey. canvas:' + this.canvas + 'pauseMenuDiv:' + this.pauseMenuDiv);
+  }
+  var df = document.createDocumentFragment();
+
+  this.canvas = Dom.ce('canvas', df);
+  this.canvas.id = 'canvas';
+
+  this.pauseMenuDiv = Dom.ce('div', df);
+  this.pauseMenuDiv.id = 'pauseMenu';
+  document.body.appendChild(df);
+  document.body.classList.add('canvasPage');
+
+  var metaViewport = document.head.querySelector('meta[name="viewport"]');
+  this.oldMetaViewportContent = metaViewport.content;
+  metaViewport.content = 'width=device-width, user-scalable=no';
+
+  this.refreshPauseMenu();
+
+  this.sfx = new SoundFx();
+  this.sfx.setListenerXYZ(0, 0, 2);
+
+  // On-event sound unlocker for iOS.
+  var resumeButton = document.querySelector('#resumeButton');
+  var boundUnlock = this.unlockIosSound.bind(this);
+  resumeButton.addEventListener('touchend', boundUnlock);
+  resumeButton.addEventListener('touchstart', boundUnlock);
+  this.canvas.addEventListener('touchend', boundUnlock);
+  this.canvas.addEventListener('touchstart', boundUnlock);
+
+  // prevent default on a lot of pinch and scroll events on mobile
+  this.canvas.addEventListener('touchstart', Dom.pd);
+  this.canvas.addEventListener('touchmove', Dom.pd);
+  this.canvas.addEventListener('touchend', Dom.pd);
+  window.addEventListener('scroll', Dom.pd);
+
+  // load level
+  this.jsonObj = this.fileTree.getFile(this.levelDataPath);
+};
+
+/**
+ * It seems that a drag won't work. There has to be a clean tap.
+ * For now, I'll unlock every time there's a touchend.
+ */
+ScreenPage.prototype.unlockIosSound = function() {
+  this.sfx.sound(0, 0, 0, 0.001, 0, 0, 0.001, 1, 1, 'sine');
+  this.iosSoundUnlocked++;
+};
+
+ScreenPage.prototype.exitDoc = function() {
+  if (!this.canvas || !this.pauseMenuDiv) {
+    throw Error('nodes should be truthy. canvas:' + this.canvas + 'pauseMenuDiv:' + this.pauseMenuDiv);
+  }
+  window.removeEventListener("scroll", Dom.pd);
+
+  if (this.screen) {
+    this.maybeSaveLevel();
+    this.screen.setScreenListening(false);
+  }
+  document.body.removeChild(this.canvas);
+  document.body.removeChild(this.pauseMenuDiv);
+  document.body.classList.remove('canvasPage');
+  this.canvas = null;
+  this.pauseMenuDiv = null;
+  this.animationId = 0;
+
+  var metaViewport = document.head.querySelector('meta[name="viewport"]');
+  metaViewport.content = this.oldMetaViewportContent;
+  this.oldMetaViewportContent = null;
+};
+
+ScreenPage.prototype.maybeSaveLevel = function() {
+  throw "implement maybeSaveLevel";
+};
+
+ScreenPage.prototype.setPaused = function(paused) {
+  this.paused = paused;
+  if (this.screen) this.screen.setPaused(this.paused);
+};
+
+ScreenPage.prototype.refreshPauseMenu = function() {
+  throw 'implement refreshPauseMenu';
+};
+
+ScreenPage.prototype.appendTitle = function(df) {
+  var e = Dom.ce('div', df, 'gameTitle');
+  e.innerHTML = Strings.textToHtml(this.gameTitle);
+};
+ScreenPage.prototype.appendFullScreenButton = function(df) {
+  var e = Dom.ce('button', df, 'smallButton');
+  e.id = 'fullScreenButton';
+  e.innerHTML = Strings.textToHtml('full screen');
+};
+
+ScreenPage.prototype.appendResumeButton = function(df, opt_text) {
+  var e = Dom.ce('button', df, 'mainButton');
+  e.id = 'resumeButton';
+  e.innerHTML = Strings.textToHtml(opt_text || 'resume');
+};
+
+ScreenPage.prototype.setPauseMenuContent = function(df) {
+  this.pauseMenuDiv.innerHTML = '';
+  this.pauseMenuDiv.appendChild(df);
+};
+
+ScreenPage.prototype.onShaderTextChange = function(vertexShaderText, fragmentShaderText) {
+  if (!this.canvas) {
+    console.log('onShaderTextChange with no this.canvas');
+    return;
+  }
+  if (this.renderer) {
+    // already did this
+    console.log('renderer already exists');
+    return;
+  }
+
+  var gl = getWebGlContext(this.canvas, {
+    alpha: false,
+    antialias: true
+  });
+  var vs = compileShader(gl, vertexShaderText, gl.VERTEX_SHADER);
+  var fs = compileShader(gl, fragmentShaderText, gl.FRAGMENT_SHADER);
+  var program = createProgram(gl, vs, fs);
+  gl.enable(gl.DEPTH_TEST);
+  gl.useProgram(program);
+  this.renderer = new Renderer(this.canvas, gl, program);
+
+  this.maybeCreateScreen();
+};
+
+ScreenPage.prototype.maybeCreateScreen = function() {
+  throw 'implement maybeCreateScreen';
+};
+
+ScreenPage.prototype.requestAnimation = function() {
+  if (!this.animationId) {
+    this.animationId = requestAnimationFrame(this.animateFrameFn, this.canvas);
+  }
+};
+
+ScreenPage.prototype.animateFrame = function(startTimeMs) {
+  if (!this.animationId) {
+    return;
+  }
+  if (!this.canvas) {
+    console.log('animateFrame with no this.canvas');
+    return;
+  }
+
+  this.animationId = 0;
+  this.renderer.resize().clear();
+  this.screen.setScreenListening(true);
+  this.screen.drawScreen(1, startTimeMs);
+};
+
+ScreenPage.prototype.requestFullScreen = function() {
+  Dom.requestFullScreen();
+  this.requestAnimation();
+};
