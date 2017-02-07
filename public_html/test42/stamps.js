b0579295b8b171e0719b8b5892ef8e4f272f3643
Aaron Whyte <aaron.whyte@gmail.com>
2017-02-06 21:57:12 -0800

    nest for test42:multiplayer controls

---
+++ public_html/test42/stamps.js
@@ -0,0 +1,25 @@
+/**
+ * Collection of common stamps for this game, besides the ones the Spirit classes own.
+ * @constructor
+ */
+function Stamps() {
+}
+
+Stamps.create = function(renderer) {
+  var stamps = new Stamps();
+  var models = new Models();
+  function stamp(model) {
+    return model.createModelStamp(renderer.gl);
+  }
+  // basic geometry
+  stamps.circleStamp = stamp(RigidModel.createCircle(32));
+  stamps.tubeStamp = stamp(RigidModel.createTube(32));
+  stamps.cylinderStamp = stamp(RigidModel.createCylinder(32));
+  stamps.lineStamp = stamp(RigidModel.createCylinder(9));
+
+  // HUD icons
+  stamps.playerPauseStamp = stamp(models.getPlayerPause());
+
+  return stamps;
+};
+
---
+++ public_html/test42/test42playscreen.js
@@ -0,0 +1,120 @@
+/**
+ * @constructor
+ * @extends {Test42BaseScreen}
+ */
+function Test42PlayScreen(controller, canvas, renderer, stamps, sfx) {
+  Test42BaseScreen.call(this, controller, canvas, renderer, stamps, sfx);
+
+  this.camera = new Camera(0.2, 0.6, 30);
+  this.updateViewMatrix();
+  this.renderer.setViewMatrix(this.viewMatrix);
+
+  this.hudViewMatrix = new Matrix44();
+
+  var self = this;
+}
+Test42PlayScreen.prototype = new Test42BaseScreen();
+Test42PlayScreen.prototype.constructor = Test42PlayScreen;
+
+Test42PlayScreen.ANT_RAD = 1.2;
+
+Test42PlayScreen.MAX_UNDO_DEPTH = 20000;
+
+Test42PlayScreen.prototype.updateHudLayout = function() {
+  this.canvasCuboid.setToCanvas(this.canvas);
+};
+
+Test42PlayScreen.prototype.getCamera = function() {
+  return this.camera;
+};
+
+Test42PlayScreen.prototype.setScreenListening = function(listen) {
+  if (listen == this.listening) return;
+  var fsb, rb, i;
+  Test42BaseScreen.prototype.setScreenListening.call(this, listen);
+  if (listen) {
+    for (i = 0; i < this.listeners.vals.length; i++) {
+      this.listeners.vals[i].startListening();
+    }
+
+    fsb = document.querySelector('#fullScreenButton');
+    fsb.addEventListener('click', this.fullScreenFn);
+    fsb.addEventListener('touchend', this.fullScreenFn);
+
+    rb = document.querySelector('#resumeButton');
+    rb.addEventListener('click', this.pauseDownFn);
+    rb.addEventListener('touchend', this.pauseDownFn);
+
+    this.canvas.addEventListener('mousemove', this.keyTipRevealer);
+    window.addEventListener('keydown', this.keyTipRevealer);
+
+  } else {
+    for (i = 0; i < this.listeners.vals.length; i++) {
+      this.listeners.vals[i].stopListening();
+    }
+
+    fsb = document.querySelector('#fullScreenButton');
+    fsb.removeEventListener('click', this.fullScreenFn);
+    fsb.removeEventListener('touchend', this.fullScreenFn);
+
+    rb = document.querySelector('#resumeButton');
+    rb.removeEventListener('click', this.pauseDownFn);
+    rb.removeEventListener('touchend', this.pauseDownFn);
+
+    this.canvas.removeEventListener('mousemove', this.keyTipRevealer);
+    window.removeEventListener('keydown', this.keyTipRevealer);
+  }
+  this.listening = listen;
+};
+
+Test42PlayScreen.prototype.createDefaultWorld = function() {
+  this.world.setChangeRecordingEnabled(true);
+  this.tileGrid.drawTerrainPill(Vec2d.ZERO, Vec2d.ZERO, 20, 1);
+  var ants = 24;
+  for (var a = 0; a < ants; a++) {
+    this.addItem(Test42BaseScreen.MenuItem.ANT, new Vec2d(0, 15).rot(2 * Math.PI * a / ants), 2 * Math.PI * a / ants);
+  }
+
+  var ants = 12;
+  for (var a = 0; a < ants; a++) {
+    this.addItem(Test42BaseScreen.MenuItem.ANT, new Vec2d(0, 10).rot(2 * Math.PI * a / ants), 2 * Math.PI * a / ants);
+  }
+};
+
+Test42PlayScreen.prototype.handleInput = function () {
+  // TOOD players
+};
+
+Test42PlayScreen.prototype.drawScene = function() {
+  this.renderer.setViewMatrix(this.viewMatrix);
+  var startTime = performance.now();
+  this.drawSpirits();
+  stats.add(STAT_NAMES.DRAW_SPIRITS_MS, performance.now() - startTime);
+
+  this.drawTiles();
+  this.splasher.draw(this.renderer, this.world.now);
+  this.drawHud();
+
+  // Animate whenever this thing draws.
+  if (!this.paused) {
+    this.controller.requestAnimation();
+  }
+};
+
+Test42PlayScreen.prototype.drawHud = function() {
+  this.hudViewMatrix.toIdentity()
+      .multiply(this.mat44.toScaleOpXYZ(
+          2 / this.canvas.width,
+          -2 / this.canvas.height,
+          1))
+      .multiply(this.mat44.toTranslateOpXYZ(-this.canvas.width/2, -this.canvas.height/2, 0));
+  this.renderer.setViewMatrix(this.hudViewMatrix);
+
+  this.updateHudLayout();
+  this.renderer.setBlendingEnabled(true);
+  this.renderer.setBlendingEnabled(false);
+};
+
+Test42PlayScreen.prototype.isPlaying = function() {
+  return true;
+};
