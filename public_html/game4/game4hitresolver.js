9d651762e67004a76c9faa2c540cb7c862b5fc4d
Aaron <aaron.whyte@gmail.com>
2017-12-03 13:49:58 -0800

    game4 hitresolver code move
    It's out of the screen and into the custom hitresolver.
    There are no behavior changes though.

--- public_html/game4/game4hitresolver.js
+++ public_html/game4/game4hitresolver.js
@@ -10,8 +10,76 @@ function Game4HitResolver(screen, bouncer) {
 }
 
 Game4HitResolver.prototype.resolveHit = function(time, collisionVec, b0, b1) {
-  // TODO a bunch of other stuff like damage and death
   this.bouncer.resolveHit(time, collisionVec, b0, b1);
+
+  var vec = Vec2d.alloc();
+  var mag = vec.set(b1.vel).subtract(b0.vel).projectOnto(collisionVec).magnitude();
+  var pos = this.getHitPos(time, collisionVec, b0, b1, vec);
+  var otherBody, otherSpirit;
+
+  var playerBody = this.screen.bodyIfSpiritType(Game4BaseScreen.SpiritType.PLAYER, b0, b1);
+  if (playerBody) {
+    var playerSpirit = this.screen.getSpiritForBody(playerBody);
+    var antBody = this.screen.bodyIfSpiritType(Game4BaseScreen.SpiritType.ANT, b0, b1);
+    if (antBody) {
+      this.screen.killPlayerSpirit(playerSpirit);
+    }
+    if (!antBody) {
+      // TODO: thump on wall hit, not on "else"
+      this.screen.sounds.wallThump(pos, mag * 10);
+    }
+  }
+
+  var bulletBody = this.screen.bodyIfSpiritType(Game4BaseScreen.SpiritType.BULLET, b0, b1);
+  if (bulletBody) {
+    var bulletSpirit = this.screen.getSpiritForBody(bulletBody);
+    otherBody = this.screen.otherBody(bulletBody, b0, b1);
+    otherSpirit = this.screen.getSpiritForBody(otherBody);
+    if (!otherSpirit) {
+      // wall?
+      bulletSpirit.onHitWall(mag, pos);
+    } else if (otherSpirit.type === Game4BaseScreen.SpiritType.ANT) {
+      otherSpirit.onPlayerBulletHit(bulletSpirit.damage);
+      bulletSpirit.onHitEnemy(mag, pos);
+    } else if (otherSpirit.type === Game4BaseScreen.SpiritType.BULLET) {
+      bulletSpirit.onHitOther(mag, pos);
+      otherSpirit.onHitOther(mag);
+    } else {
+      bulletSpirit.onHitOther(mag, pos);
+    }
+  }
+
+  var abb = this.screen.bodyIfSpiritType(Game4BaseScreen.SpiritType.ACTIVATOR_BULLET, b0, b1);
+  if (abb) {
+    var abbs = this.screen.getSpiritForBody(abb);
+    otherBody = this.screen.otherBody(abb, b0, b1);
+    otherSpirit = this.screen.getSpiritForBody(otherBody);
+    if (otherSpirit && otherSpirit.isActivatable()) {
+      abbs.onHitActivatable(otherSpirit, pos);
+    } else {
+      abbs.onHitOther(pos);
+    }
+  }
+
+  var tbb = this.screen.bodyIfSpiritType(Game4BaseScreen.SpiritType.TRACTOR_BULLET, b0, b1);
+  if (tbb) {
+    var tbbs = this.screen.getSpiritForBody(tbb);
+    tbbs.onHitOther(pos);
+  }
+
+  var ebb = this.screen.bodyIfSpiritType(Game4BaseScreen.SpiritType.ENERGY_BULLET, b0, b1);
+  if (ebb) {
+    var ebbs = this.screen.getSpiritForBody(ebb);
+    otherBody = this.screen.otherBody(ebb, b0, b1);
+    otherSpirit = this.screen.getSpiritForBody(otherBody);
+    if (otherSpirit && otherSpirit.getEnergyCapacity()) {
+      ebbs.onHitEnergizable(otherSpirit, pos);
+    } else {
+      ebbs.onHitOther(pos);
+    }
+  }
+  vec.free();
+
 };
 
 Game4HitResolver.prototype.getHitPos = function(time, collisionVec, b0, b1, out) {
