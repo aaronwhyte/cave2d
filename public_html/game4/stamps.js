9da3683506acd870d2c0e3b00be3bf175653a809
Aaron <aaron.whyte@gmail.com>
2018-02-05 19:02:04 -0800

    var to let

--- public_html/game4/stamps.js
+++ public_html/game4/stamps.js
@@ -7,10 +7,10 @@ function Stamps() {
 
 Stamps.create = function(renderer) {
 
-  var stamps = new Stamps();
-  var glyphs = new Glyphs(new GlyphMaker(0.4, 1.2));
+  let stamps = new Stamps();
+  let glyphs = new Glyphs(new GlyphMaker(0.4, 1.2));
   glyphs.initModels();
-  var models = new Models(glyphs);
+  let models = new Models(glyphs);
   function stamp(model) {
     return model.createModelStamp(renderer.gl);
   }
@@ -19,15 +19,20 @@ Stamps.create = function(renderer) {
   stamps.septagonStamp = stamp(RigidModel.createCircle(7));
   stamps.squareStamp = stamp(RigidModel.createSquare());
   stamps.tubeStamp = stamp(RigidModel.createTube(32));
+
+
   stamps.cylinderStamp = stamp(RigidModel.createCylinder(32));
+  stamps.cylinderBatchDrawer = batchDrawer(RigidModel.createCylinder(32));
+
+
   stamps.lineStamp = stamp(RigidModel.createCylinder(9));
 
   // fancier stuff
-  var model = RigidModel.createCube()
+  let model = RigidModel.createCube()
       .createQuadrupleTriangleModel().createQuadrupleTriangleModel().sphereize(Vec4.ZERO, 1);
-  for (var i = 0; i < model.vertexes.length; i++) {
-    var v = model.vertexes[i];
-    var height = 1 - (v.position.getZ() / 2 + 0.5);
+  for (let i = 0; i < model.vertexes.length; i++) {
+    let v = model.vertexes[i];
+    let height = 1 - (v.position.getZ() / 2 + 0.5);
     v.color.scale1(height);
   }
   stamps.sphereLowPoly = stamp(model);
