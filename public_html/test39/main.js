8d3e2d0f47928fbe7530f7d03eed2c3824d6d19e
Aaron <aaron.whyte@gmail.com>
2018-02-17 23:22:07 -0800

    deleted test39
    because it was a pain to maintain.
    Also removed main-page refs to 40 and 38, which have been deleted for a while.

--- public_html/test39/main.js
+++
@@ -1,9 +0,0 @@
-var app;
-function main() {
-  var path = ['test39'];
-  var fileTree = new FileTree(new JsonStorage({}));
-  app = new Test39App('Test 39', path, fileTree,
-      'vertex-shader.txt', 'fragment-shader.txt');
-  app.start();
-}
-
