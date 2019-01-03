984ad696da7e03c5270f8282e5b19efeac1f27dd
Aaron <aaron.whyte@gmail.com>
2019-01-02 23:42:59 -0800

    placeholder playerselectpage

---
+++ public_html/game6/pages/playerselectpage.js
@@ -0,0 +1,43 @@
+/**
+ * Add/remove players and controls.
+ * @param {PlayApp} app
+ * @constructor
+ * @extends (Page)
+ */
+function PlayerSelectPage(app) {
+  Page.call(this);
+  this.app = app;
+}
+PlayerSelectPage.prototype = new Page();
+PlayerSelectPage.prototype.constructor = PlayerSelectPage;
+
+PlayerSelectPage.prototype.enterDoc = function() {
+  Page.prototype.enterDoc.call(this);
+  this.exitPointerLock();
+  let df = document.createDocumentFragment();
+
+  this.div = Dom.ce('div', df);
+  this.div.innerHTML = 'player select page goes here';
+  document.body.appendChild(df);
+  document.body.classList.add('PlayerSelectPage');
+
+  let metaViewport = document.head.querySelector('meta[name="viewport"]');
+  this.oldMetaViewportContent = metaViewport.content;
+  metaViewport.content = 'width=device-width';
+
+  this.sfx = new SoundFx();
+  this.sfx.setListenerXYZ(0, 0, 5);
+};
+
+PlayerSelectPage.prototype.exitDoc = function() {
+  Page.prototype.exitDoc.call(this);
+  document.body.removeChild(this.div);
+  document.body.classList.remove('PlayerSelectPage');
+  this.canvas = null;
+  this.pauseMenuDiv = null;
+  this.animationId = 0;
+
+  let metaViewport = document.head.querySelector('meta[name="viewport"]');
+  metaViewport.content = this.oldMetaViewportContent;
+  this.oldMetaViewportContent = null;
+};
